6059307833c41f5b563d58411767d26ac9a8be87
federicopignatelli <federicopignatelli@icloud.com>
2023-12-04 14:48:21 +0100

    es0

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
+import java.util.Random;
 
 public class Main {
+    public static void main(String[] args) {
+        stampaNumeri();
+    }
 
+    public static void stampaNumeri() {
+
+        Random random = new Random();
+        int lunghezzaArray = 5;
+
+        int[] numeriCasuali = new int[lunghezzaArray];
+
+        for (int i = 0; i < lunghezzaArray; i++) {
+            numeriCasuali[i] = random.nextInt(10) + 1;
+        }
+
+        System.out.print("Numeri casuali generati: ");
+        for (int numero : numeriCasuali) {
+            System.out.print(numero + " ");
+        }
+    }
 }
